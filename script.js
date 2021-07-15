e86b48e51ddf1c7d3508711da8e43daaa3be44d6
gabrieladipoggio <gabrieladipoggio@hotmail.com>
2021-07-15 00:19:42 -0300

    Changes energy bar

--- script.js
+++ script.js
@@ -351,19 +351,19 @@ function start() {
 
         function energia() {
             if (energiaAtual==3) {
-                $("#energia").css("background-image", "url(./imgs/energia3.png)");
+                $("#energia").css("background-image", "url(./imgs/heart3.png)");
             }
         
             if (energiaAtual==2) {
-                $("#energia").css("background-image", "url(./imgs/energia2.png)");
+                $("#energia").css("background-image", "url(./imgs/heart2.png)");
             }
         
             if (energiaAtual==1) {
-                $("#energia").css("background-image", "url(./imgs/energia1.png)");
+                $("#energia").css("background-image", "url(./imgs/heart1.png)");
             }
 
             if (energiaAtual==0) {
-                $("#energia").css("background-image", "url(./imgs/energia0.png)");
+                $("#energia").css("background-image", "url(./imgs/heart0.png)");
                 //Game Over
                 gameOver();
             }
